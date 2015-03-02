626542545a62324c73f34fc2719f0bff24a5edb2
Spencer Salisbury <spencer.salisbury@gmail.com>
2015-03-02 09:00:25 -0700

    Started the navier project.

---
+++ projects/navier/config.f90
@@ -0,0 +1,17 @@
+module config
+implicit none
+private
+
+integer,parameter::sp=selected_real_kind(6)
+integer,parameter::dp=selected_real_kind(15)
+integer,parameter::ep=selected_real_kind(18)
+integer,parameter::qp=selected_real_kind(30)
+integer,parameter::wp=sp
+
+public wp,disp_wp
+
+contains
+    subroutine disp_wp
+        write(*,*)wp
+    end subroutine
+end module config
